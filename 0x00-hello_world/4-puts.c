4ab15372efbabf207c364efe730cb75b1e79cdfa
root <bearden.lucas@gmail.com>
2023-01-28 09:06:47 -0800

    commit for task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 /**
- * Description: Main - Starting point for program execution
- * Return: 0 if successful
+ * Description: Main - Starting point for program execution.
+ * Return: 0 if successful.
  */
-
-
 int main(void)
 {
-
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
